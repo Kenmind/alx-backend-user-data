e72de6a249975d31afad088a652cbbe928b1eb14
Kenmind <kennedykalaluka04@gmail.com>
2022-08-01 10:51:26 +0200

    fix view one user

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,13 +27,11 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None and request.current_user() is None:
         abort(404)
-    if user is None:
-        abort(404)
     if user_id == 'me':
         if request.current_user is None:
             abort(404)
         else:
-            return jsonify(request.current_user.to_join())
+            return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
