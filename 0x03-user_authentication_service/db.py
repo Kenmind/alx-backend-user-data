d5722c841faefbe63843f8443161148bae71c2ea
Kenmind <kennedykalaluka04@gmail.com>
2022-08-02 16:54:09 +0200

    Update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -61,3 +61,19 @@ class DB:
         if res is None:
             raise NoResultFound()
         return res
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates an existing user
+        """
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update_info = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                update_info[getattr(User, key)] = value
+            else:
+                raise ValueError()
+            self._session.query(User).filter(User.id == user_id).update(
+                    update_info, synchronize_session=False,)
+            self._session.commit()
